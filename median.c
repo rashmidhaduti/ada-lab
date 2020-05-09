ddfebfeb9a52c7265d6ac193883288b7524d6af4
rashmidhaduti <53811679+rashmidhaduti@users.noreply.github.com>
2020-05-09 14:57:50 +0530

    Add files via upload

--- median.c
+++ median.c
@@ -1,30 +1,61 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
-{
-int a[100],b [100],n1,n2,j=0,k=0,i=0,c[200],med;
-printf("Print size of array1:");
-scanf("%d",&n1);
-printf("Print size of array2:");
-scanf("%d",&n2);
-printf("\nEnter sorted array1:");
-for(int z=0;z<n1;z++)
-scanf("%d",&a[z]);
-printf("\nEnter sorted array2:");
-for(int z=0;z<n2;z++)
-scanf("%d",&b[z]);
-while(i<n1&&j<n2)
-{
-if(a[i]<b[j])
-  c[k++]=a[i++];
-else
-  c[k++]=b[j++];
-}
-while(i<n1)
-c[k++]=a[i++];
-while(j<n2)
-c[k++]=b[j++];
-med=(int)(n1+n2)/2;
-printf("\nMedian of merged array is %d",c[med]);
-return;
+int getMedian(int ar1[], int ar2[], int n) 
+{ 
+    int i = 0;  
+    int j=0;
+    int count; 
+    int m1 = -1, m2 = -1; 
+
+    for (count = 0; count <= n; count++) 
+    { 
+        if (i == n) 
+        { 
+            m1 = m2; 
+            m2 = ar2[0]; 
+            break; 
+        } 
+  
+        else if (j == n) 
+        { 
+            m1 = m2; 
+            m2 = ar1[0]; 
+            break; 
+        } 
+  
+        if (ar1[i] < ar2[j]) 
+        { 
+            m1 = m2;  
+            m2 = ar1[i]; 
+            i++; 
+        } 
+        else
+        { 
+            m1 = m2; 
+            m2 = ar2[j]; 
+            j++; 
+        } 
+    } 
+  
+    return (m1 + m2)/2; 
+} 
+int main() 
+{ 
+    int ar1[10],ar2[10],i;
+    int n1,n2;
+    printf("Enter size of array 1\n");
+   scanf("%d",&n1);
+    printf("Enter array:1 elements \n");
+   for(i=0;i<n1;i++)
+   scanf("%d",&ar1[i]);
+   printf("Enter size of array 2\n");
+   scanf("%d",&n2);
+   printf("Enter array:2 elements\n");
+   for(i=0;i<n2;i++)
+   scanf("%d",&ar2[i]);
+    if (n1 == n2) 
+        printf("Median is %d", getMedian(ar1, ar2, n1)); 
+    else
+        printf("Doesn't work for arrays of unequal size"); 
+    getchar(); 
+    return 0; 
 }
